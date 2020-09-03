2a80de713e407274e4ffbb2a5a1b8d1d0bd4c995
parkdain <parkdain@localhost>
2020-09-03 10:15:55 +0900

    working add 메세지 추가

--- src/org/opentutorials/javatutorials/eclipse/Helloworld.java
+++ src/org/opentutorials/javatutorials/eclipse/Helloworld.java
@@ -5,7 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		 System.out.println("Hello world");
 		 System.out.println("Hi Worl");
-		 System.out.println("working");
+		 System.out.println("working add");
 	}
 
 } 
